aaac7c5ab8f3999e8ef9e458d89babdf91f76c33
Philip Ogboh <philipogboh60@gmail.com>
2024-12-07 12:08:36 +0100

    removed proxy setup

--- src/context/axios.js
+++ src/context/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "/api",
+  baseURL: import.meta.env.VITE_DEV_API_BASE_URL,
 });
 axiosInstance.interceptors.request.use((config) => {
   const token = localStorage.getItem("token");
